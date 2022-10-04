d162a187aa25a8f49faafe46f24d23dac606623f
Paulo Martinez <paulomartinez1999@gmail.com>
2022-10-04 15:10:06 -0500

    creacion del login

---
+++ ProyectoMuni/CadenaC/cadena.php
@@ -0,0 +1,11 @@
+<?php
+$db = '(DESCRIPTION =
+(ADDRESS = (PROTOCOL = TCP)
+(HOST = 192.168.69.5)(PORT = 1521))
+(CONNECT_DATA =
+(SERVER = DEDICATED)
+(SERVICE_NAME = cabildo)))';
+$username1='GADSPP';
+$password1='FEEDBACK17';
+
+?>
---
+++ ProyectoMuni/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="css/index.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <header></header>
+
+    <div class="formulario" >
+        <div class="titulo">
+            <h3>LogIn</h3>
+        </div>
+        <form action="">
+            <div class="UserName" >
+                <label for="user">Usuario</label>
+                <input type="text" name="user" >
+            </div>
+            <div class="UserPass" >
+                <label for="pass">Contraseña</label>
+                <input type="text" name="pass" >
+            </div>
+            <div>
+                <input type="button" value="LogIn">
+            </div>
+        </form>
+    </div>
+
+    <footer></footer>
+</body>
+</html>
